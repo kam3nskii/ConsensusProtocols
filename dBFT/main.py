585faec6407ef7885f9cc8567e13f72f3027e505
Denis Kamenskii <dekamenskii@mail.ru>
2023-05-12 23:39:41 +0300

    dBFT: Add safe binary Byzantine consensus

--- dBFT/main.py
+++ dBFT/main.py
@@ -1,13 +1,12 @@
 from dslib import Context, Message, Node
 
 from typing import List
-import random
 
 
 class MSGS():
-    init = "INIT"
-    est = "EST"
-    aux = "AUX"
+    init   = "INIT"
+    est    = "EST"
+    aux    = "AUX"
     result = "RESULT"
 
 
@@ -16,97 +15,127 @@ def broadcast(ctx, nodes, msg):
         ctx.send(msg, node)
 
 
-def createMsgEst(value, round):
-    return Message(MSGS.est, {'value': value,
-                              'round': round})
-
-
-def createMsgAux(values, round):
-    return Message(MSGS.aux, {'bin_values': list(values),
-                              'round': round})
-
-
-def decide(ctx, value, decided):
+def decide(ctx, r, value, decided):
     if not decided:
-        ctx.send_local(Message(MSGS.result, {'value': value}))
+        ctx.send_local(Message(MSGS.result,
+                               {'value': value, 'round': r}))
     return True
 
 
-class dBFTNode(Node):
-    def __init__(self, node_id: str, nodes: List[str], seed: int, faulty_count: int):
+def BV_Broadcast(ctx, nodes, r, value, broadcasted):
+    if r not in broadcasted:
+        broadcasted[r] = set()
+    if value not in broadcasted[r]:
+        broadcasted[r].add(value)
+        broadcast(ctx, nodes, Message(MSGS.est,
+                                      {'value': value, 'round': r}))
+
+
+def BV_Delivery(ctx, nodes, r, value, bin_values):
+    if r not in bin_values:
+        bin_values[r] = set()
+    if value not in bin_values[r]:
+        bin_values[r].add(value)
+        broadcast(ctx, nodes, Message(MSGS.aux,
+                                      {'bin_values': list(bin_values[r]), 'round': r}))
+
+
+def receiveEstMessage(storage, r, value, sender):
+    if r not in storage:
+        storage[r] = {}
+    if value not in storage[r]:
+        storage[r][value] = set()
+    storage[r][value].add(sender)
+    sendersCnt = len(storage[r][value])
+    return sendersCnt
+
+
+def receiveAuxMessage(storage, r, values, sender):
+    if r not in storage:
+        storage[r] = {0: set(), 1: set()}
+    for value in values:
+        storage[r][value].add(sender)
+
+
+def checkAuxMessage(storage, r, values, bin_values, min_senders_cnt):
+    if r not in bin_values:
+        bin_values[r] = set()
+    for value in values:
+        sendersCnt = len(storage[r][value])
+        if (sendersCnt < min_senders_cnt) or (value not in bin_values[r]):
+            return False
+    return True
+
+
+class SafeBBC(Node):
+    def __init__(self, node_id: str, nodes: List[str], faulty_count: int):
         self._id = node_id
         self._nodes = nodes
         self._f_count = faulty_count
-        random.seed(seed + int(self._id))
 
+        self._decided = False
         self._est = None
         self._round = 0
         self._bin_values = {}
-        self._b = None
+
         self._received_ests = {}
+        self._broadcasted_ests = {}
+
         self._received_auxs = {}
-        self._decided = False
 
     def on_local_message(self, msg: Message, ctx: Context):
+        # ------------------------------------------------------------------------------
+        # INIT
         if msg.type == MSGS.init:
             self._est = msg['value']
             self._round += 1
-            broadcast(ctx, self._nodes, createMsgEst(self._est, self._round))
+            BV_Broadcast(ctx, self._nodes, self._round,
+                         self._est, self._broadcasted_ests)
+        # ------------------------------------------------------------------------------
 
     def on_message(self, msg: Message, sender: str, ctx: Context):
+
+        # ------------------------------------------------------------------------------
+        # EST
         if msg.type == MSGS.est:
             r = msg['round']
             value = msg['value']
 
-            if r not in self._received_ests:
-                self._received_ests[r] = {}
-            if value not in self._received_ests[r]:
-                self._received_ests[r][value] = set()
-            if sender in self._received_ests[r][value]:
-                return
+            sendersCnt = receiveEstMessage(
+                self._received_ests, r, value, sender)
 
-            self._received_ests[r][value].add(sender)
+            if sendersCnt >= self._f_count + 1:
+                BV_Broadcast(ctx, self._nodes, r, value,
+                             self._broadcasted_ests)
 
-            cnt = len(self._received_ests[r][value])
-            if cnt == self._f_count + 1:
-                broadcast(ctx, self._nodes, createMsgEst(value, self._round))
-            if cnt == 2 * self._f_count + 1:
-                if r not in self._bin_values:
-                    self._bin_values[r] = set()
-                self._bin_values[r].add(value)
-                broadcast(ctx, self._nodes, createMsgAux(
-                    self._bin_values[r], self._round))
-
+            if sendersCnt >= 2 * self._f_count + 1:
+                BV_Delivery(ctx, self._nodes, r, value, self._bin_values)
+        # ------------------------------------------------------------------------------
+        # AUX
         if msg.type == MSGS.aux:
             r = msg['round']
             values = set(msg['bin_values'])
 
-            if r not in self._received_auxs:
-                self._received_auxs[r] = {0: set(), 1: set()}
+            receiveAuxMessage(self._received_auxs, r, values, sender)
 
-            alreadyDelivered = True
-            for value in values:
-                if sender not in self._received_auxs[r][value]:
-                    alreadyDelivered = False
-                    self._received_auxs[r][value].add(sender)
-            if alreadyDelivered:
+            if r != self._round:
                 return
 
-            if values.issubset(self._bin_values):
-                for value in values:
-                    if len(self._received_auxs[r][value]) < len(self._nodes) - self._f_count:
-                        return
-
-                self._b = r % 2
+            if checkAuxMessage(self._received_auxs, self._round, values,
+                               self._bin_values, len(self._nodes) - self._f_count):
+                b = self._round % 2
                 if len(values) == 1:
-                    value = values.pop()
-                    if value == self._b:
-                        self._est = value
-                        self._decided = decide(ctx, self._est, self._decided)
-                    else:
-                        self._est = self._b
+                    self._est = values.pop()
+                    if self._est == b:
+                        self._decided = decide(
+                            ctx, self._round, self._est, self._decided)
+                else:
+                    self._est = b
+
                 self._round += 1
-                broadcast(ctx, self._nodes, createMsgEst(self._est, self._round))
+                BV_Broadcast(ctx, self._nodes, self._round,
+                             self._est, self._broadcasted_ests)
+        # ------------------------------------------------------------------------------
 
     def on_timer(self, timer_name: str, ctx: Context):
         pass
--- dBFT/test/src/main.rs
+++ dBFT/test/src/main.rs
@@ -17,7 +17,7 @@ use dslib::test::{TestResult, TestSuite};
 
 #[derive(Serialize)]
 struct MessageInit {
-    value: u8,
+    value: u64,
 }
 
 #[derive(Copy, Clone)]
@@ -44,7 +44,7 @@ fn build_system(config: &TestConfig) -> System<JsonMessage> {
     for node_id in node_ids.iter() {
         let node = config.node_factory.build(
             node_id,
-            (node_id, node_ids.clone(), config.seed, config.faulty_count),
+            (node_id, node_ids.clone(), config.faulty_count),
             config.seed,
         );
         sys.add_node(rc!(refcell!(node)));
@@ -60,47 +60,108 @@ fn get_local_messages(sys: &System<JsonMessage>, node: &str) -> Vec<JsonMessage>
         .collect::<Vec<_>>()
 }
 
+fn send_init_messages(sys: &mut System<JsonMessage>, init_values: &Vec<u64>) {
+    for (idx, init_value) in init_values.iter().enumerate() {
+        sys.send_local(
+            JsonMessage::from("INIT", &MessageInit { value: *init_value }),
+            &format!("{}", idx),
+        );
+    }
+}
+
+fn check_consensus(
+    sys: &mut System<JsonMessage>,
+    nodes: &Vec<String>,
+    mut expected_result: Option<u64>,
+) -> TestResult {
+    for node in nodes.iter() {
+        let mut messages = get_local_messages(&sys, &node);
+
+        if messages.len() == 0 {
+            let res = sys.step_until_local_message(&node);
+            assume!(res.is_ok(), format!("Node {}: No messages returned!", node))?;
+            messages = get_local_messages(&sys, &node);
+        }
+
+        assume!(
+            messages.len() == 1,
+            format!("Node {}: Wrong number of messages!", node)
+        )?;
+        assume!(
+            messages[0].tip == "RESULT",
+            format!("Node {}: Wrong message type!", node)
+        )?;
+
+        let data: Value = serde_json::from_str(&messages[0].data).unwrap();
+        let value = data["value"].as_u64().unwrap();
+        if expected_result.is_none() {
+            expected_result = Some(value);
+        }
+        assume!(
+            value == expected_result.unwrap(),
+            format!(
+                "Node {}: returned {} instead of {}",
+                node,
+                value,
+                expected_result.unwrap()
+            )
+        )?;
+    }
+    Ok(true)
+}
+
 // TESTS -----------------------------------------------------------------------
 
 fn test_simple(config: &TestConfig) -> TestResult {
     let mut sys = build_system(config);
+    let nodes = sys.get_node_ids();
 
-    let mut node_values = Vec::new();
-    for _i in 0..config.node_count {
-        node_values.push(sys.gen_range(0..2));
-    }
-    for (idx, node_value) in node_values.iter().enumerate() {
-        sys.send_local(
-            JsonMessage::from("INIT", &MessageInit { value: *node_value }),
-            &format!("{}", idx),
-        );
+    let mut init_values = Vec::new();
+    for _ in nodes.iter() {
+        init_values.push(sys.gen_range(0..2));
     }
 
-    let mut result: u8 = 0;
-    for i in 0..config.node_count {
-        let node_id = format!("{}", i);
-        let mut messages = get_local_messages(&sys, &node_id);
+    send_init_messages(&mut sys, &init_values);
 
-        if messages.len() == 0 {
-            let res = sys.step_until_local_message(&node_id);
-            assume!(res.is_ok(), format!("Node {}: No messages returned!", i))?;
-            messages = get_local_messages(&sys, &node_id);
-        }
+    check_consensus(&mut sys, &nodes, None)
+}
 
-        assume!(messages[0].tip == "RESULT", format!("Node {}: Wrong message type!", i))?;
+fn test_all_one(config: &TestConfig) -> TestResult {
+    let mut sys = build_system(config);
+    let nodes = sys.get_node_ids();
 
-        let data: Value = serde_json::from_str(&messages[0].data).unwrap();
-        let value = data["value"].as_u64().unwrap() as u8;
-        if i == 0 {
-            result = value;
-        }
-        assume!(
-            value == result,
-            format!("Node {}: returned {} instead of {}", i, value, result)
-        )?;
+    let mut init_values = Vec::new();
+    init_values.resize(nodes.len(), 1);
+    send_init_messages(&mut sys, &init_values);
+
+    check_consensus(&mut sys, &nodes, Some(1))
+}
+
+fn test_all_zero(config: &TestConfig) -> TestResult {
+    let mut sys = build_system(config);
+    let nodes = sys.get_node_ids();
+
+    let mut init_values = Vec::new();
+    init_values.resize(nodes.len(), 0);
+    send_init_messages(&mut sys, &init_values);
+
+    check_consensus(&mut sys, &nodes, Some(0))
+}
+
+fn test_half_half(config: &TestConfig) -> TestResult {
+    let mut sys = build_system(config);
+    let nodes = sys.get_node_ids();
+
+    let mut init_values = Vec::new();
+    init_values.resize(nodes.len(), 1);
+    let half = nodes.len() / 2;
+    for i in 0..half {
+        init_values[i] = 0;
     }
 
-    Ok(true)
+    send_init_messages(&mut sys, &init_values);
+
+    check_consensus(&mut sys, &nodes, None)
 }
 
 // MAIN ------------------------------------------------------------------------
@@ -139,7 +200,7 @@ fn main() {
     init_logger(LevelFilter::Trace);
 
     env::set_var("PYTHONPATH", format!("{}/python", args.dslib_path));
-    let node_factory = PyNodeFactory::new(&args.impl_path, "dBFTNode");
+    let node_factory = PyNodeFactory::new(&args.impl_path, "SafeBBC");
     let config = TestConfig {
         node_count: args.node_count,
         faulty_count: args.faulty_count,
@@ -148,9 +209,15 @@ fn main() {
     };
 
     let mut tests = TestSuite::new();
-    if test.is_none() || test.unwrap() == "simple" {
-        tests.add("TEST SIMPLE", test_simple, config);
-    }
 
-    tests.run();
+    tests.add("TEST SIMPLE", test_simple, config);
+    tests.add("TEST ALL ONE", test_all_one, config);
+    tests.add("TEST ALL ZERO", test_all_zero, config);
+    tests.add("TEST HALF/HALF", test_half_half, config);
+
+    if test.is_none() {
+        tests.run();
+    } else {
+        tests.run_test(test.unwrap());
+    }
 }
